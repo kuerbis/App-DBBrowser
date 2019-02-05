590416f854254be9d8377a367e2a0e15136058b7
kuerbis <cuer2s@gmail.com>
2019-02-05 18:56:48 +0100

    Update documentation

--- lib/App/DBBrowser/DB.pm
+++ lib/App/DBBrowser/DB.pm
@@ -5,7 +5,7 @@ use warnings;
 use strict;
 use 5.008003;
 
-our $VERSION = '2.054';
+our $VERSION = '2.055';
 
 #use bytes; # required
 use Scalar::Util qw( looks_like_number );
@@ -422,7 +422,7 @@ App::DBBrowser::DB - Database plugin documentation.
 
 =head1 VERSION
 
-Version 2.054
+Version 2.055
 
 =head1 DESCRIPTION
 
