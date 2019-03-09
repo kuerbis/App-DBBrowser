a30a02d72cebc96f361384326dc4f55a8fdca13d
kuerbis <cuer2s@gmail.com>
2019-03-09 07:46:50 +0100

    Attach/detach database: fix file name hash key

--- lib/App/DBBrowser.pm
+++ lib/App/DBBrowser.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use 5.008003;
 
-our $VERSION = '2.068';
+our $VERSION = '2.069';
 
 use Encode                qw( decode );
 use File::Basename        qw( basename );
@@ -693,7 +693,7 @@ App::DBBrowser - Browse SQLite/MySQL/PostgreSQL databases and their tables inter
 
 =head1 VERSION
 
-Version 2.068
+Version 2.069
 
 =head1 DESCRIPTION
 
