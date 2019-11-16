471fe5c39b0942c26ddd3b1e8c545472c6d6bbbe
kuerbis <cuer2s@gmail.com>
2019-11-16 15:05:18 +0100

    Update documentation. Coming back from a not completed "create table": keep track of sheet index

--- lib/App/DBBrowser.pm
+++ lib/App/DBBrowser.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use 5.010001;
 
-our $VERSION = '2.222';
+our $VERSION = '2.223';
 
 use File::Basename        qw( basename );
 use File::Spec::Functions qw( catfile catdir );
@@ -711,7 +711,7 @@ App::DBBrowser - Browse SQLite/MySQL/PostgreSQL databases and their tables inter
 
 =head1 VERSION
 
-Version 2.222
+Version 2.223
 
 =head1 DESCRIPTION
 
