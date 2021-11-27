63938fe904bcc75115b4dcac4d844a4f7532fff3
kuerbis <cuer2s@gmail.com>
2021-11-27 10:15:52 +0100

    use JSON::MaybeXS instead of JSON

--- lib/App/DBBrowser/DB.pm
+++ lib/App/DBBrowser/DB.pm
@@ -5,7 +5,7 @@ use warnings;
 use strict;
 use 5.010001;
 
-our $VERSION = '2.288';
+our $VERSION = '2.289';
 
 #use bytes; # required
 use Scalar::Util qw( looks_like_number );
@@ -377,7 +377,7 @@ App::DBBrowser::DB - Database plugin documentation.
 
 =head1 VERSION
 
-Version 2.288
+Version 2.289
 
 =head1 DESCRIPTION
 
